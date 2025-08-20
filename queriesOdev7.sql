b3d03e58671dc5d69d51f210dbbb8d3a7c26399a
Mehmet Aşar Kıribiş <mehmet_asar_@hotmail.com>
2025-08-20 19:11:35 +0300

    Ödev 7 gönderildi

---
+++ queriesOdev7.sql
@@ -0,0 +1,22 @@
+select rating, count(*) 
+from film group by rating 
+order by rating;
+
+
+select replacement_cost, count(*) 
+from film group by replacement_cost 
+having count(*) > 50 order by count (*) desc, replacement_cost;
+
+
+select store_id, count(*)
+from customer
+group by store_id
+order by store_id;
+
+
+select country_id, count (*)
+from city
+group by country_id
+order by count(*) desc
+limit 1;
+
