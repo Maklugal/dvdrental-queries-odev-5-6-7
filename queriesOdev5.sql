7311b45685792eb9de7ec283dc725440f128234a
Mehmet Aşar Kıribiş <mehmet_asar_@hotmail.com>
2025-08-20 15:04:36 +0300

    Ödev 5 gönderildi

---
+++ queriesOdev5.sql
@@ -0,0 +1,5 @@
+select title from film where title like '%n' order by length desc, title limit 5;
+
+select title from film where title like '%n' order by length, title limit 5 offset 5; 
+
+select customer_id, first_name, store_id from customer where store_id =1 order by last_name desc, first_name desc limit 4;
