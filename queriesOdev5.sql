af240a42e48efdb92290e67b0abff10cfa68039c
Mehmet Aşar Kıribiş <mehmet_asar_@hotmail.com>
2025-08-20 15:07:02 +0300

    Ödev 5 revize

--- queriesOdev5.sql
+++ queriesOdev5.sql
@@ -2,4 +2,4 @@ select title from film where title like '%n' order by length desc, title limit 5
 
 select title from film where title like '%n' order by length, title limit 5 offset 5; 
 
-select customer_id, first_name, store_id from customer where store_id =1 order by last_name desc, first_name desc limit 4;
+select customer_id, first_name, store_id from customer where store_id =1 order by last_name desc, first_name desc limit 4; 
